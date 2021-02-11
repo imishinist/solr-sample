0a23a41c59e0b8b543aa2e4f25a89462b9004add
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-11 21:14:53 +0900

    update documents

--- src/main/java/Solrj.java
+++ src/main/java/Solrj.java
@@ -2,15 +2,17 @@ import model.Recipe;
 import config.Config;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class Solrj {
-    public static void main(String[] args) throws IOException, SolrServerException {
-        Config config = Config.init(System.getenv());
 
+    private static List<Recipe> getRecipe() {
         Recipe r = new Recipe();
         r.id = "hogehoge1";
         r.name = "回鍋肉";
@@ -21,10 +23,17 @@ public class Solrj {
         r.external_info_url = null;
         r.external_info_title = null;
 
+        return Collections.singletonList(r);
+    }
+
+    public static void main(String[] args) throws IOException, SolrServerException {
+        Config config = Config.init(System.getenv());
+
         String solrUrl = String.format("%s/solr/recipe", config.solrServer);
-        final SolrClient solrClient = new HttpSolrClient.Builder(solrUrl).build();
+        final ConcurrentUpdateSolrClient solrClient = new ConcurrentUpdateSolrClient.Builder(solrUrl).build();
         try {
-            UpdateResponse updateResponse = solrClient.addBean(r);
+            List<Recipe> rs = Solrj.getRecipe();
+            UpdateResponse updateResponse = solrClient.addBeans(rs);
 
             if (updateResponse.getStatus() == 0) {
                 updateResponse = solrClient.commit();
