1f8aa4f124fd5e601ddae80e1452dfa493961e4f
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-11 22:36:24 +0900

    getter to func

--- src/main/java/Solrj.kt
+++ src/main/java/Solrj.kt
@@ -8,29 +8,28 @@ import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient
 import java.util.ArrayList
 
 object Solrj {
-    private val recipe: List<Recipe?>
-        private get() {
-            return listOf(
-                    Recipe(
-                            "hogehoge1",
-                            "回鍋肉",
-                            "Chinese",
-                            listOf("きゃべつ"),
-                            listOf("半分"),
-                            listOf("きゃべつを切る。"),
-                    ),
-                    Recipe(
-                            "hogehoge2",
-                            "カレー",
-                            "Japanese",
-                            listOf("たまねぎ", "豚肉"),
-                            listOf("半分", "200g"),
-                            listOf("たまねぎを炒める"),
-                            "https://www.youtube.com/watch?v=BcKNwo7bzJ4",
-                            "我が家のカレーがこれになってしまったと何度も言われたほどウマい、市販のルーで作る【至高のカレー】"
-                    )
-            )
-        }
+    private fun getRecipe(): List<Recipe> {
+        return listOf(
+                Recipe(
+                        "hogehoge1",
+                        "回鍋肉",
+                        "Chinese",
+                        listOf("きゃべつ"),
+                        listOf("半分"),
+                        listOf("きゃべつを切る。"),
+                ),
+                Recipe(
+                        "hogehoge2",
+                        "カレー",
+                        "Japanese",
+                        listOf("たまねぎ", "豚肉"),
+                        listOf("半分", "200g"),
+                        listOf("たまねぎを炒める"),
+                        "https://www.youtube.com/watch?v=BcKNwo7bzJ4",
+                        "我が家のカレーがこれになってしまったと何度も言われたほどウマい、市販のルーで作る【至高のカレー】"
+                )
+        )
+    }
 
     @Throws(IOException::class, SolrServerException::class)
     @JvmStatic
@@ -39,8 +38,7 @@ object Solrj {
         val solrUrl = String.format("%s/solr/recipe", config.solrServer)
         val solrClient = ConcurrentUpdateSolrClient.Builder(solrUrl).build()
         try {
-            val rs = recipe
-            var updateResponse = solrClient.addBeans(rs)
+            var updateResponse = solrClient.addBeans(getRecipe())
             if (updateResponse.status == 0) {
                 updateResponse = solrClient.commit()
                 System.out.printf("%s %s", updateResponse.status, updateResponse.response)
