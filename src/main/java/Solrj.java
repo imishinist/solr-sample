ae18a4f4a666c926fa970849af934ad3132d56e7
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-11 21:20:43 +0900

    add sample data

--- src/main/java/Solrj.java
+++ src/main/java/Solrj.java
@@ -7,12 +7,14 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Solrj {
 
     private static List<Recipe> getRecipe() {
+        List<Recipe> ret = new ArrayList<>();
         Recipe r = new Recipe();
         r.id = "hogehoge1";
         r.name = "回鍋肉";
@@ -22,8 +24,22 @@ public class Solrj {
         r.genre = "Chinese";
         r.external_info_url = null;
         r.external_info_title = null;
+        ret.add(r);
 
-        return Collections.singletonList(r);
+        Recipe curry = new Recipe();
+        curry.id = "hogehoge2";
+        curry.name = "カレー";
+        curry.ingredients.add("たまねぎ");
+        curry.ingredients.add("豚肉");
+        curry.ingredients_amount.add("半分");
+        curry.ingredients_amount.add("200g");
+        curry.steps.add("たまねぎを炒める");
+        curry.genre = "Japanese";
+        curry.external_info_url = "https://www.youtube.com/watch?v=BcKNwo7bzJ4";
+        curry.external_info_title = "我が家のカレーがこれになってしまったと何度も言われたほどウマい、市販のルーで作る【至高のカレー】";
+        ret.add(curry);
+
+        return ret;
     }
 
     public static void main(String[] args) throws IOException, SolrServerException {
